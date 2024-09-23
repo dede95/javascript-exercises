2d146845a275f87030395979d5a49e5d5367f1d7
dede95 <29232994+dede95@users.noreply.github.com>
2024-09-23 23:21:09 +0100

    Create function to concat string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let longString = ""
+    for (let i = 0; i < num; i++) {
+        longString = longString.concat(string)
+    }
+
+    return longString
 
 };
+
+//run in browser using `node <script name>`
+console.log(repeatString('hey', 6))
 
 // Do not edit below this line
 module.exports = repeatString;
