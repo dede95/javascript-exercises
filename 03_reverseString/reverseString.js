0f34f41ace0f3b7d80f60e58bef5ac785574d6df
dede95 <29232994+dede95@users.noreply.github.com>
2024-09-23 23:48:19 +0100

    Function for reversing string - uses array and array methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    
+    return string.split("").reverse().join("")
 };
+
+console.log(reverseString("hey"))
 
 // Do not edit below this line
 module.exports = reverseString;
