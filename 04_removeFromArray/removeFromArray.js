f0d3cce763930f44a685430481df9a41c86129a4
dede95 <29232994+dede95@users.noreply.github.com>
2024-09-26 01:03:57 +0100

    Complete exercise to remove item(s) from array - utilising for loops and array method forEach or filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    // Removes number(s) from array 
+
+    let newArray = []
+
+    array.forEach((item) => {
+        if (num.includes(item) === false){
+            // Using `include` for array instead of `item in num`
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
